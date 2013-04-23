465a3178ffad41cd51c89d7128bd338df5d46abd
Jason Dreisbach <jtdreisb@gmail.com>
2013-04-22 21:03:35 -0700

    move away from applescript

---
+++ aurora/AULoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  AULoginViewController.h
+//  Aurora
+//
+//  Created by Jason Dreisbach on 4/22/13.
+//  Copyright (c) 2013 Jason Dreisbach. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AULoginViewController : NSViewController
+
+@end
---
+++ aurora/AULoginViewController.m
@@ -0,0 +1,27 @@
+//
+//  AULoginViewController.m
+//  Aurora
+//
+//  Created by Jason Dreisbach on 4/22/13.
+//  Copyright (c) 2013 Jason Dreisbach. All rights reserved.
+//
+
+#import "AULoginViewController.h"
+
+@interface AULoginViewController ()
+
+@end
+
+@implementation AULoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
